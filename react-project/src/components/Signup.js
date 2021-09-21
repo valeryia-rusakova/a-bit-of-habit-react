3e82a8fcd66b669f403adcc39cb503fb40d5b580
valeria-rusakova-learnpython <valeria.rusakova.learnpython@gmail.com>
2021-09-21 17:01:55 +0300

    Refactoring sign-up page

--- react-project/src/components/Signup.js
+++ react-project/src/components/Signup.js
@@ -1,46 +1,15 @@
 import {Component} from "react";
 import {AccountCircle, Email, Lock} from "@material-ui/icons";
 import {East} from "@mui/icons-material";
-import styled from "styled-components";
 import {
     FooterWrapper,
     FormFieldWrapper,
     HeaderWrapper,
     SpanWrapper,
-    InputWrapper
+    InputWrapper,
+    SignupForm,
+    SignupButton
 } from "../css-common";
-
-
-const FormWrapper = styled.div`
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    width: 409px;
-    height: 433px;
-    transform: translate(-50%, -50%);
-`
-const ButtonWrapper = styled.button`
-    background-color: #EDA794;
-    width: 109px;
-    height: 52px;
-    float: right;
-    display: flex;
-    align-items: center;
-    margin-block: 5px;
-    border-radius: 8px;
-    border: none;
-    p { 
-        display: block;
-        margin: 8px;
-        text-decoration: none;
-        color: #F8F9F9;
-        font-family: Montserrat;
-        font-style: normal;
-        font-weight: 400;
-        font-size: 14px;
-        line-height: 17px;
-    } 
-`
 
 
 export default class Signup extends Component{
@@ -75,33 +44,33 @@ export default class Signup extends Component{
 
     render(){
          return(
-            <FormWrapper container onSubmit={this.handleSubmit}>
+            <SignupForm container onSubmit={this.handleSubmit}>
                 <HeaderWrapper>
                      Sign up
                 </HeaderWrapper>
                 <FormFieldWrapper>
-                    <SpanWrapper><AccountCircle style={{ color: '#F8F9F9'}}/></SpanWrapper>
+                    <SpanWrapper><AccountCircle/></SpanWrapper>
                     <InputWrapper type={"text"} placeholder="Enter username"/>
                 </FormFieldWrapper>
                 <FormFieldWrapper>
-                    <SpanWrapper><Email style={{ color: '#F8F9F9'}}/></SpanWrapper>
+                    <SpanWrapper><Email/></SpanWrapper>
                     <InputWrapper type={"email"} placeholder="Enter email"/>
                 </FormFieldWrapper>
                 <FormFieldWrapper>
-                    <SpanWrapper><Lock style={{ color: '#F8F9F9' }} /></SpanWrapper>
+                    <SpanWrapper><Lock/></SpanWrapper>
                     <InputWrapper type={"password"} placeholder="Enter password"/>
                 </FormFieldWrapper>
                 <FormFieldWrapper>
-                    <SpanWrapper><Lock style={{ color: '#F8F9F9' }} /></SpanWrapper>
+                    <SpanWrapper><Lock/></SpanWrapper>
                     <InputWrapper type={"password"} placeholder="Confirm password"/>
                 </FormFieldWrapper>
                 <FooterWrapper>
-                    <p>Already have an account? <a href={'/login'} style={{color: '#EDA794'}}> Sign in</a></p>
+                    <p>Already have an account? <a href={'/login'}> Sign in</a></p>
                 </FooterWrapper>
-                <ButtonWrapper>
-                    <p>Sign up</p><East style={{ color: '#F8F9F9' }}/>
-                </ButtonWrapper>
-            </FormWrapper>
+                <SignupButton>
+                    <p>Sign up</p><East/>
+                </SignupButton>
+            </SignupForm>
         )
     }
 }

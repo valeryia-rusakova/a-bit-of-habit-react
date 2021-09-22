56ebee85b4e5895b43c67748c2fc0a79a8570f20
valeria-rusakova-learnpython <valeria.rusakova.learnpython@gmail.com>
2021-09-22 22:52:37 +0300

    Change registration page with responsive design

--- react-project/src/components/Signup.js
+++ react-project/src/components/Signup.js
@@ -1,25 +1,30 @@
 import {Component} from "react";
 import {AccountCircle, Email, Lock} from "@material-ui/icons";
 import {East} from "@mui/icons-material";
+import {Container, Grid} from "@material-ui/core";
+import * as React from "react";
 import {
-    FooterWrapper,
     FormFieldWrapper,
     HeaderWrapper,
     SpanWrapper,
     InputWrapper,
-    SignupForm,
-    SignupButton
+    FooterLink,
+    LinkWrapper,
+    FooterButton,
+    SignupButton,
+    SignupForm
 } from "../css-common";
+
 
 
 export default class Signup extends Component{
     constructor(props) {
         super(props);
-            this.handleUsernameChange = this.handleUsernameChange.bind(this);
-            this.handleEmailChange = this.handleEmailChange.bind(this);
-            this.handlePasswordChange = this.handlePasswordChange.bind(this);
-            this.handleSubmit = this.handleSubmit.bind(this);
-            this.state = {
+        this.handleUsernameChange = this.handleUsernameChange.bind(this);
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.state = {
             username: '',
             email: '',
             password: '',
@@ -43,34 +48,54 @@ export default class Signup extends Component{
     }
 
     render(){
-         return(
-            <SignupForm container onSubmit={this.handleSubmit}>
-                <HeaderWrapper>
-                     Sign up
-                </HeaderWrapper>
-                <FormFieldWrapper>
-                    <SpanWrapper><AccountCircle/></SpanWrapper>
-                    <InputWrapper type={"text"} placeholder="Enter username"/>
-                </FormFieldWrapper>
-                <FormFieldWrapper>
-                    <SpanWrapper><Email/></SpanWrapper>
-                    <InputWrapper type={"email"} placeholder="Enter email"/>
-                </FormFieldWrapper>
-                <FormFieldWrapper>
-                    <SpanWrapper><Lock/></SpanWrapper>
-                    <InputWrapper type={"password"} placeholder="Enter password"/>
-                </FormFieldWrapper>
-                <FormFieldWrapper>
-                    <SpanWrapper><Lock/></SpanWrapper>
-                    <InputWrapper type={"password"} placeholder="Confirm password"/>
-                </FormFieldWrapper>
-                <FooterWrapper>
-                    <p>Already have an account? <a href={'/login'}> Sign in</a></p>
-                </FooterWrapper>
-                <SignupButton>
-                    <p>Sign up</p><East/>
-                </SignupButton>
-            </SignupForm>
+        return(
+            <Container maxWidth="xs">
+                <SignupForm container>
+                    <Grid item xs={12}>
+                        <HeaderWrapper>
+                            Sign up
+                        </HeaderWrapper>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <FormFieldWrapper>
+                            <SpanWrapper><AccountCircle/></SpanWrapper>
+                            <InputWrapper type={"text"} placeholder=" Enter username"/>
+                        </FormFieldWrapper>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <FormFieldWrapper>
+                            <SpanWrapper><Email/></SpanWrapper>
+                            <InputWrapper type={"email"} placeholder=" Enter email"/>
+                        </FormFieldWrapper>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <FormFieldWrapper>
+                            <SpanWrapper><Lock/></SpanWrapper>
+                            <InputWrapper type={"password"} placeholder=" Enter password"/>
+                        </FormFieldWrapper>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <FormFieldWrapper>
+                            <SpanWrapper><Lock/></SpanWrapper>
+                            <InputWrapper type={"password"} placeholder=" Confirm password"/>
+                        </FormFieldWrapper>
+                    </Grid>
+                    <Grid container >
+                        <FooterLink item xs={6}>
+                            <LinkWrapper>
+                                <p>Already have an account?<br/>
+                                <a href={'/login'}> Sign in</a>
+                                </p>
+                            </LinkWrapper>
+                        </FooterLink>
+                    <FooterButton item xs={6}>
+                        <SignupButton>
+                            <p>Sign up</p><East/>
+                        </SignupButton>
+                    </FooterButton>
+                    </Grid>
+                </SignupForm>
+            </Container>
         )
+        }
     }
-}

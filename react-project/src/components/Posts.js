bde3db3eedd143e73d09917086a08e2e61fd5059
valeria-rusakova-learnpython <valeria.rusakova.learnpython@gmail.com>
2021-10-31 17:03:38 +0300

    Create mock file for posts

--- react-project/src/components/Posts.js
+++ react-project/src/components/Posts.js
@@ -2,6 +2,35 @@ import {Container, Grid} from "@material-ui/core";
 import * as React from "react";
 import {PostAuthor, PostBody, PostButton, PostContainer, PostItem, PostTitle} from "../css/posts";
 import {Link} from "react-router-dom";
+import {PostsList} from "../mocks/PostsList";
+
+
+const postItems = PostsList.map((post)=>{
+    return(
+    <PostItem item xs={12}>
+         <PostAuthor item xs={12}>
+             <p>{post.author}</p>
+         </PostAuthor>
+         <PostTitle item xs={12}>
+             <p>
+                 {post.title}
+             </p>
+         </PostTitle>
+         <PostBody item>
+             <p>
+                 {post.body}
+             </p>
+         </PostBody>
+         <Grid item>
+            <PostButton>
+                <p>
+                    <Link to="/post">Read more</Link>
+                </p>
+            </PostButton>
+         </Grid>
+    </PostItem>
+    );
+});
 
 function Posts() {
     return (
@@ -9,78 +38,7 @@ function Posts() {
             <Grid item>
                  <Container maxWidth="lg">
                      <PostContainer container>
-                         <PostItem item xs={12}>
-                             <PostAuthor item xs={12}>
-                                 <p>Valerya Rusakova</p>
-                             </PostAuthor>
-                             <PostTitle item xs={12}>
-                                 <p>
-                                     42 healthy habits that won't consume titanic efforts,
-                                     but will transform your life
-                                 </p>
-                             </PostTitle>
-                             <PostBody item>
-                                 <p>
-                                     To make your life better, you don't have to move mountains and change
-                                     dramatically from Monday. It is enough to acquire good habits that will step by step make your everyday life brighter and more fulfilling.
-                                 </p>
-                             </PostBody>
-                             <Grid item>
-                                <PostButton>
-                                    <p>
-                                        <Link to="/post">Read more</Link>
-                                    </p>
-                                </PostButton>
-                             </Grid>
-                         </PostItem>
-                         <PostItem item xs={12}>
-                             <PostAuthor item xs={12}>
-                                 <p>Valerya Rusakova</p>
-                             </PostAuthor>
-                             <PostTitle item xs={12}>
-                                 <p>
-                                     42 healthy habits that won't consume titanic efforts,
-                                     but will transform your life
-                                 </p>
-                             </PostTitle>
-                             <PostBody item>
-                                 <p>
-                                     To make your life better, you don't have to move mountains and change
-                                     dramatically from Monday. It is enough to acquire good habits that will step by step make your everyday life brighter and more fulfilling.
-                                 </p>
-                             </PostBody>
-                             <Grid item>
-                                <PostButton>
-                                    <p>
-                                         <Link to="/post">Read more</Link>
-                                    </p>
-                                </PostButton>
-                             </Grid>
-                         </PostItem>
-                         <PostItem item xs={12}>
-                             <PostAuthor item xs={12}>
-                                 <p>Valerya Rusakova</p>
-                             </PostAuthor>
-                             <PostTitle item xs={12}>
-                                 <p>
-                                     42 healthy habits that won't consume titanic efforts,
-                                     but will transform your life
-                                 </p>
-                             </PostTitle>
-                             <PostBody item>
-                                 <p>
-                                     To make your life better, you don't have to move mountains and change
-                                     dramatically from Monday. It is enough to acquire good habits that will step by step make your everyday life brighter and more fulfilling.
-                                 </p>
-                             </PostBody>
-                             <Grid item>
-                                <PostButton>
-                                    <p>
-                                         <Link to="/post">Read more</Link>
-                                    </p>
-                                </PostButton>
-                             </Grid>
-                         </PostItem>
+                         {postItems}
                      </PostContainer>
                  </Container>
             </Grid>
---
+++ react-project/src/mocks/PostsList.js
@@ -0,0 +1,37 @@
+export const PostsList = [
+    {
+        author: 'Valeryia Rusakova',
+        title: '42 healthy habits that won\'t consume titanic efforts,\n' +
+            'but will transform your life',
+        body: 'To make your life better, you don\'t have to move mountains and change ' +
+            'dramatically from Monday. It is enough to acquire good habits that will step by step make your everyday life brighter and more fulfilling',
+    },
+    {
+        author: 'Amy Farha',
+        title: '42 healthy habits that won\'t consume titanic efforts,\n' +
+            'but will transform your life',
+        body: 'Et harum quidem rerum facilis est et expedita distinctio, quis nostrum exercitationem ullam corporis ' +
+            'suscipit laboriosam, ut et voluptates repudiandae sint et molestiae non recusandae',
+    },
+    {
+        author: 'Wade Warren',
+        title: '42 healthy habits that won\'t consume titanic efforts,\n' +
+            'but will transform your life',
+        body: 'Quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur,' +
+            'vel illum, sunt in culpa qui officia deserunt mollit anim id est laborum?',
+    },
+    {
+        author: 'Chris Jackson',
+        title: '42 healthy habits that won\'t consume titanic efforts,\n' +
+            'but will transform your life',
+        body: 'Nemo enim ipsam voluptatem, consectetur adipiscing elit,' +
+            ' qui dolorem eum fugiat, quo voluptas nulla pariatur.',
+    },
+    {
+        author: 'Chris Jackson',
+        title: '42 healthy habits that won\'t consume titanic efforts,\n' +
+            'but will transform your life',
+        body: 'Nemo enim ipsam voluptatem, qui in ea voluptate velit esse, ' +
+            'quam nihil molestiae consequatur, vel illum, qui...',
+    },
+]

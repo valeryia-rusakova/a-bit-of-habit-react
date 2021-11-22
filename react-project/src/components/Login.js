4e908ec27100044a2bf5b1487f6079ff9d73050b
valeria-rusakova-learnpython <valeria.rusakova.learnpython@gmail.com>
2021-11-22 18:09:23 +0300

    Change Login & Profile component to interact with Redux

--- react-project/src/components/Login.js
+++ react-project/src/components/Login.js
@@ -1,80 +1,95 @@
 import * as React from 'react';
-import {Component} from "react";
-import {Container, Grid} from "@material-ui/core";
-import {AccountCircle, Lock} from "@material-ui/icons";
-import {East} from "@mui/icons-material";
+import {Component, useState} from "react";
+
+import { connect } from 'react-redux';
+
+import {Redirect} from "react-router-dom";
+import { login } from '../actions/auth';
 import {
-    FooterButton, FooterLink,
+    FooterButton,
+    FooterLink,
     FormFieldWrapper,
     HeaderWrapper,
     InputWrapper,
-    LinkWrapper,
-    LoginButton,
+    LinkWrapper, LoginButton,
     LoginForm,
     SpanWrapper
 } from "../css/auth";
-import {Link} from "react-router-dom";
+import {Container, Grid} from "@material-ui/core";
+import {AccountCircle, Lock} from "@material-ui/icons";
+import {East} from "@mui/icons-material";
 
+const Login = ({ login, isAuthenticated }) => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
 
-export default class Login extends Component{
-    constructor(props) {
-        super(props);
-            this.handleUsernameChange = this.handleUsernameChange.bind(this);
-            this.handlePasswordChange = this.handlePasswordChange.bind(this);
-            this.handleSubmit = this.handleSubmit.bind(this);
-            this.state = {
-            username: '',
-            password: '',
-        };
+    const { email, password } = formData;
+
+    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    const onSubmit = e => {
+        e.preventDefault();
+
+        login(email, password);
+    };
+
+    if (isAuthenticated) {
+        return <Redirect to='/profile' />
     }
 
-    handleSubmit(event) {
-        event.preventDefault();
-    }
+    return(
+        <Container maxWidth="xs">
+            <LoginForm onSubmit={e => onSubmit(e)}>
+                <Grid item xs={12}>
+                    <HeaderWrapper>
+                        Sign in
+                    </HeaderWrapper>
+                </Grid>
+                <Grid item xs={12}>
+                    <FormFieldWrapper>
+                        <SpanWrapper><AccountCircle/></SpanWrapper>
+                        <InputWrapper
+                            type='email'
+                            name='email'
+                            value={email}
+                            onChange={e => onChange(e)}
+                            required
+                            placeholder="    Enter username"/>
+                    </FormFieldWrapper>
+                </Grid>
+                <Grid item xs={12}>
+                    <FormFieldWrapper>
+                        <SpanWrapper><Lock/></SpanWrapper>
+                        <InputWrapper
+                            type='password'
+                            name='password'
+                            value={password}
+                            onChange={e => onChange(e)}
+                            minLength='6'
+                            required
+                            placeholder="     Enter password"/>
+                    </FormFieldWrapper>
+                </Grid>
+                <Grid container >
+                    <FooterLink item xs={8}>
+                        <LinkWrapper>
+                            <p>New in A bit of Habit? <a href={'/signup'}> Create account</a></p>
+                        </LinkWrapper>
+                    </FooterLink>
+                    <FooterButton item xs={4}>
+                        <LoginButton type='submit'><East/></LoginButton>
+                    </FooterButton>
+                </Grid>
+            </LoginForm>
+        </Container>
+)
 
-    handleUsernameChange(event) {
-        this.setState({username: event.target.value});
-    }
-
-    handlePasswordChange(event) {
-        this.setState({password: event.target.value});
-    }
-
-    render(){
-         return(
-            <Container maxWidth="xs">
-                <LoginForm container>
-                    <Grid item xs={12}>
-                        <HeaderWrapper>
-                            Sign in
-                        </HeaderWrapper>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <FormFieldWrapper>
-                            <SpanWrapper><AccountCircle/></SpanWrapper>
-                            <InputWrapper type={"text"} placeholder="    Enter username"/>
-                        </FormFieldWrapper>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <FormFieldWrapper>
-                            <SpanWrapper><Lock/></SpanWrapper>
-                            <InputWrapper type={"password"} placeholder="     Enter password"/>
-                        </FormFieldWrapper>
-                    </Grid>
-                    <Grid container >
-                        <FooterLink item xs={8}>
-                            <LinkWrapper>
-                                <p>New in A bit of Habit? <Link to="/signup"> Create account</Link></p>
-                            </LinkWrapper>
-                        </FooterLink>
-                        <FooterButton item xs={4}>
-                            <LoginButton>
-                                    <Link to="/signup"><East/></Link>
-                            </LoginButton>
-                        </FooterButton>
-                    </Grid>
-                </LoginForm>
-            </Container>
-        )
-    }
 }
+
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { login })(Login);

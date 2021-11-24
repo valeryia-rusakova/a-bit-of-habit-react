429581939c4a6dd40ee35aac74ad60d52dfe0351
valeria-rusakova-learnpython <valeria.rusakova.learnpython@gmail.com>
2021-11-24 17:34:36 +0300

    Create config.json, add API_HOST variable

--- react-project/src/actions/auth.js
+++ react-project/src/actions/auth.js
@@ -2,6 +2,7 @@ import {
     LOGIN_SUCCESS,
     LOGIN_FAIL, SIGNUP_SUCCESS, SIGNUP_FAIL, LOGOUT,
 } from "./types";
+import configData from "../config.json";
 import axios from "axios";
 
 
@@ -13,7 +14,7 @@ export const login = (email, password) => async dispatch => {
     };
     const body = JSON.stringify({ email, password });
     try {
-        const res = await axios.post(`http://localhost:8000/users/login/`, body, config);
+        const res = await axios.post(`${configData.API_HOST}/users/login/`, body, config);
 
         dispatch({
             type: LOGIN_SUCCESS,
@@ -36,7 +37,7 @@ export const signup = (username, email, password) => async dispatch => {
     const body = JSON.stringify({ username, email, password});
 
     try {
-        const res = await axios.post(`http://localhost:8000/users/`, body, config);
+        const res = await axios.post(`${process.env.apiHost}/users/`, body, config);
 
         dispatch({
             type: SIGNUP_SUCCESS,

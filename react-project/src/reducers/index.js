66f0bbefeec6b1021951ea518c1bc64a3e6ee521
valeria-rusakova-learnpython <valeria.rusakova.learnpython@gmail.com>
2021-12-07 05:08:01 +0300

    Create actions & reducers related to habits

--- react-project/src/reducers/index.js
+++ react-project/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import auth from './auth';
 import posts from "./posts";
+import habits from "./habits";
 
 export const rootReducer = combineReducers({
     auth,
-    posts
+    posts,
+    habits
 });
